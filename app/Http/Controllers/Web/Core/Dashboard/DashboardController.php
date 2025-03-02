f4a432ab85fa879e393e92de347792cf0b2c218f
andrydwis <andry.dwi.s@gmail.com>
2025-03-02 08:42:45 +0700

    Adds dashboard statistics
    Implements dashboard statistics for total users, active users, and total watched animes.
    
    Fetches and displays key metrics on the dashboard to provide an overview of application usage.
    Uses concurrency to improve the performance of fetching data for the dashboard.

--- app/Http/Controllers/Web/Core/Dashboard/DashboardController.php
+++ app/Http/Controllers/Web/Core/Dashboard/DashboardController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Web\Core\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\AnimeWatchHistory;
+use App\Models\User;
+use Illuminate\Support\Facades\Concurrency;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('core.dashboard.index');
+        [$totalUsers, $totalActiveUsers, $totalWatchedAnimes] = Concurrency::run([
+            fn () => User::count(),
+            fn () => User::whereDate('last_login_at', today())->count(),
+            fn () => AnimeWatchHistory::whereDate('updated_at', today())->count(),
+        ]);
+
+        $data = [
+            'totalUsers' => $totalUsers,
+            'totalActiveUsers' => $totalActiveUsers,
+            'totalWatchedAnimes' => $totalWatchedAnimes,
+        ];
+
+        return view('core.dashboard.index', $data);
     }
 }
--- resources/views/core/dashboard/index.blade.php
+++ resources/views/core/dashboard/index.blade.php
@@ -1,2 +1,42 @@
 <x-layouts.core title="Dashboard">
+    <div class="flex flex-col gap-8">
+        <div class="grid grid-cols-3 gap-2">
+            <x-cards.app>
+                <flux:subheading>
+                    Total Pengguna {{ config('app.name') }}
+                </flux:subheading>
+                <flux:heading
+                    size="xl"
+                    class="flex flex-row items-center gap-2"
+                >
+                    <flux:icon.users variant="solid" />
+                    {{ $totalUsers }} Pengguna
+                </flux:heading>
+            </x-cards.app>
+            <x-cards.app>
+                <flux:subheading>
+                    Total Pengguna Aktif
+                </flux:subheading>
+                <flux:heading
+                    size="xl"
+                    class="flex flex-row items-center gap-2"
+                >
+                    <flux:icon.users variant="solid" />
+                    {{ $totalActiveUsers }} Pengguna
+                </flux:heading>
+            </x-cards.app>
+            <x-cards.app>
+                <flux:subheading>
+                    Total Anime Ditonton
+                </flux:subheading>
+                <flux:heading
+                    size="xl"
+                    class="flex flex-row items-center gap-2"
+                >
+                    <flux:icon.tv variant="solid" />
+                    {{ $totalWatchedAnimes }} Anime
+                </flux:heading>
+            </x-cards.app>
+        </div>
+    </div>
 </x-layouts.core>
