6c815dee3e30d453bf713724e35fae8e5a627633
andrydwis <andry.dwi.s@gmail.com>
2025-03-03 22:32:19 +0700

    Improves search modal layout and responsiveness.
    Enhances the search modal's layout by ensuring it fills the available height, improving responsiveness across different screen sizes.
    
    This includes adding a flex container to manage the content's vertical space and overflow, ensuring a better user experience when searching for anime.

--- resources/views/livewire/seach-modal.blade.php
+++ resources/views/livewire/seach-modal.blade.php
@@ -7,7 +7,7 @@
         name="search"
         class="md:min-h-auto h-full min-h-svh w-full !rounded-none md:h-3/4 md:!rounded-lg"
     >
-        <div class="flex flex-col gap-4">
+        <div class="flex min-h-full flex-col gap-4">
             <div>
                 <flux:heading>
                     Cari Anime
@@ -30,41 +30,46 @@
                 </x-slot>
             </flux:input>
 
-            <div class="grid grid-cols-2 gap-2">
-                @for ($i = 0; $i < 8; $i++)
-                    <x-cards.app
-                        wire:loading
-                        wire:target="search"
-                        class="aspect-video animate-pulse !bg-zinc-200 dark:!bg-zinc-600"
-                    >
-                        <div class="flex flex-col gap-2">
-                            <div class="w-full rounded-lg bg-white p-4 dark:bg-zinc-400">
+            <div class="flex h-0 flex-grow flex-col gap-2 overflow-auto rounded-lg">
+                <div class="grid grid-cols-2 gap-2">
+                    @for ($i = 0; $i < 8; $i++)
+                        <x-cards.app
+                            wire:loading
+                            wire:target="search"
+                            class="aspect-video animate-pulse !bg-zinc-200 dark:!bg-zinc-600"
+                        >
+                            <div class="flex flex-col gap-2">
+                                <div
+                                    class="w-full rounded-lg bg-white p-4 dark:bg-zinc-400">
+                                </div>
+                                <div
+                                    class="w-3/4 rounded-lg bg-white p-4 dark:bg-zinc-400">
+                                </div>
                             </div>
-                            <div class="w-3/4 rounded-lg bg-white p-4 dark:bg-zinc-400">
-                            </div>
-                        </div>
-                    </x-cards.app>
-                @endfor
-                @forelse ($animes as $anime)
-                    <x-cards.anime
-                        wire:loading.remove
-                        wire:target="search"
-                        :anime="$anime"
-                    />
-                @empty
-                    @if ($search)
-                        <x-cards.app class="col-span-2">
-                            <flux:heading>
-                                Anime Tidak Ditemukan
-                            </flux:heading>
-                            <flux:subheading>
-                                Oops! Anime yang kamu cari tidak ditemukan. Coba cari kata
-                                kunci
-                                lain.
-                            </flux:subheading>
                         </x-cards.app>
-                    @endif
-                @endforelse
+                    @endfor
+                    @forelse ($animes as $anime)
+                        <x-cards.anime
+                            wire:loading.remove
+                            wire:target="search"
+                            :anime="$anime"
+                        />
+                    @empty
+                        @if ($search)
+                            <x-cards.app class="col-span-2">
+                                <flux:heading>
+                                    Anime Tidak Ditemukan
+                                </flux:heading>
+                                <flux:subheading>
+                                    Oops! Anime yang kamu cari tidak ditemukan. Coba cari
+                                    kata
+                                    kunci
+                                    lain.
+                                </flux:subheading>
+                            </x-cards.app>
+                        @endif
+                    @endforelse
+                </div>
             </div>
         </div>
     </flux:modal>
