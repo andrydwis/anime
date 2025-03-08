a740bd17a4ec53576798f359224a5b9e1be389a4
andrydwis <andry.dwi.s@gmail.com>
2025-03-08 14:19:33 +0700

    Refactors middleware application in routes
    Updates route definitions to use array syntax for applying middleware.
    This change improves code readability and consistency across the application's route configurations.

--- routes/auth.php
+++ routes/auth.php
@@ -8,7 +8,7 @@ Route::post('register', [App\Http\Controllers\Web\Auth\RegisterController::class
 Route::get('login', [App\Http\Controllers\Web\Auth\LoginController::class, 'index'])->middleware('guest')->name('login');
 Route::post('login', [App\Http\Controllers\Web\Auth\LoginController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [App\Http\Controllers\Web\Auth\LoginController::class, 'destroy'])->middleware('auth')->name('logout');
+Route::post('logout', [App\Http\Controllers\Web\Auth\LoginController::class, 'destroy'])->middleware(['auth'])->name('logout');
 
 Route::get('auth/{driver}', [App\Http\Controllers\Web\Auth\SocialiteController::class, 'redirect'])->middleware('guest')->name('socialite.redirect');
 Route::get('auth/{driver}/callback', [App\Http\Controllers\Web\Auth\SocialiteController::class, 'callback'])->middleware('guest')->name('socialite.callback');
--- routes/public.php
+++ routes/public.php
@@ -9,20 +9,20 @@ Route::get('anime/recent', [App\Http\Controllers\Web\Public\Anime\RecentAnimeCon
 
 Route::get('anime/genre/{genre}', [App\Http\Controllers\Web\Public\Anime\GenreAnimeController::class, 'show'])->name('anime.genre.show');
 
-Route::get('anime/list', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'index'])->name('anime.list.index')->middleware('auth');
-Route::post('anime/list', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'store'])->name('anime.list.store')->middleware('auth');
-Route::get('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'show'])->name('anime.list.show')->middleware('auth');
-Route::patch('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'update'])->name('anime.list.update')->middleware('auth');
-Route::delete('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'destroy'])->name('anime.list.destroy')->middleware('auth');
+Route::get('anime/list', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'index'])->name('anime.list.index')->middleware(['auth']);
+Route::post('anime/list', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'store'])->name('anime.list.store')->middleware(['auth']);
+Route::get('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'show'])->name('anime.list.show')->middleware(['auth']);
+Route::patch('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'update'])->name('anime.list.update')->middleware(['auth']);
+Route::delete('anime/list/{playlist:slug}', [App\Http\Controllers\Web\Public\Anime\ListAnimeController::class, 'destroy'])->name('anime.list.destroy')->middleware(['auth']);
 
 Route::get('anime/{anime}', [App\Http\Controllers\Web\Public\Anime\AnimeController::class, 'show'])->name('anime.show');
-Route::get('anime/{anime}/episode/{episode}', [App\Http\Controllers\Web\Public\Episode\EpisodeController::class, 'show'])->name('anime.episode.show');
+Route::get('anime/{anime}/episode/{episode}', [App\Http\Controllers\Web\Public\Episode\EpisodeController::class, 'show'])->name('anime.episode.show')->middleware(['auth']);
 
 Route::resource('news', App\Http\Controllers\Web\Public\News\NewsController::class)->scoped(['news' => 'slug'])->only(['index', 'show']);
 Route::resource('events', App\Http\Controllers\Web\Public\Event\EventController::class)->scoped(['event' => 'slug'])->only(['index', 'show']);
 
-Route::get('profile', [App\Http\Controllers\Web\Public\Profile\ProfileController::class, 'edit'])->name('profile.edit')->middleware('auth');
-Route::patch('profile', [App\Http\Controllers\Web\Public\Profile\ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
+Route::get('profile', [App\Http\Controllers\Web\Public\Profile\ProfileController::class, 'edit'])->name('profile.edit')->middleware(['auth']);
+Route::patch('profile', [App\Http\Controllers\Web\Public\Profile\ProfileController::class, 'update'])->name('profile.update')->middleware(['auth']);
 
 Route::view('dmca', 'public.other.dmca')->name('dmca');
 Route::view('privacy-policy', 'public.other.privacy')->name('privacy-policy');
