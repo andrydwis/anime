3267f2154b06d74f93ff67f634c5a61875a7da73
andrydwis <andry.dwi.s@gmail.com>
2025-03-09 19:35:40 +0700

    Adds custom error pages
    Adds custom error pages for 404, 500, and 503 errors.
    
    These pages provide a more user-friendly experience when errors occur.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.auth>
+    <x-cards.app>
+        <flux:heading>
+            Halaman Tidak Ditemukan 🙅
+        </flux:heading>
+        <flux:subheading>
+            Oops! Halaman yang kamu cari tidak ditemukan. Coba cek url yang kamu masukkan.
+        </flux:subheading>
+    </x-cards.app>
+</x-layouts.auth>
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.auth>
+    <x-cards.app>
+        <flux:heading>
+            Ubur-ubur 🪼 iklan lele 🐟, maintenance dulu le...
+        </flux:heading>
+        <flux:subheading>
+            Saat ini website sedang dalam maintenance, coba lagi nanti ya 👋.
+        </flux:subheading>
+    </x-cards.app>
+</x-layouts.auth>
