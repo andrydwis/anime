6ff5e967fa5ff73f9efc8becef81a6020cedb1c8
andrydwis <andry.dwi.s@gmail.com>
2025-03-13 17:23:34 +0700

    Removes auth middleware from animex routes
    Removes the `auth` middleware from the `animex` routes, making the animex feature accessible to all users without requiring authentication.

--- routes/public.php
+++ routes/public.php
@@ -32,5 +32,5 @@ Route::view('privacy-policy', 'public.other.privacy')->name('privacy-policy');
 Route::view('terms-of-service', 'public.other.tos')->name('terms-of-service');
 
 // / BETA FEATURES
-Route::get('animex', [App\Http\Controllers\Web\Public\Animex\AnimexController::class, 'index'])->name('animex.index')->middleware(['auth']);
-Route::get('animex/{animex}', [App\Http\Controllers\Web\Public\Animex\AnimexController::class, 'show'])->name('animex.show')->middleware(['auth']);
+Route::get('animex', [App\Http\Controllers\Web\Public\Animex\AnimexController::class, 'index'])->name('animex.index');
+Route::get('animex/{animex}', [App\Http\Controllers\Web\Public\Animex\AnimexController::class, 'show'])->name('animex.show');
