15896be58ebb08af3def4d46166e8058b6c418f4
andrydwis <andry.dwi.s@gmail.com>
2025-03-23 11:18:53 +0700

    Removes max-md class from table cell
    Removes the `max-md:hidden` class from the table cell component.
    
    This ensures table cells are visible on all screen sizes,
    addressing an issue where they were hidden on medium-sized
    screens and smaller.

--- resources/views/components/tables/cell.blade.php
+++ resources/views/components/tables/cell.blade.php
@@ -1,4 +1,4 @@
 <td
-    {{ $attributes->class(['py-3 px-3 first:ps-0 last:pe-0 text-sm  text-zinc-500 dark:text-zinc-300 max-md:hidden']) }}>
+    {{ $attributes->class(['py-3 px-3 first:ps-0 last:pe-0 text-sm  text-zinc-500 dark:text-zinc-300']) }}>
     {{ $slot }}
 </td>
